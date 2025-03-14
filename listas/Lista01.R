a24f030a6c91863f6c79989489546c961d1ce6bf
fredbsr <fredbsr@gmail.com>
2025-03-14 09:10:43 -0300

    aula 5

---
+++ listas/Lista01.R
@@ -0,0 +1,52 @@
+# Lista 1
+
+# 1. ----
+# 1a. Atribua valor 22 à etiqueta (objeto) chamada 'pl_MDB_2017'
+# representando uma quantidade de pl's encaminhados por
+# deputados do MDB em 2017 (dado fictício)
+
+
+# 1b. Qual é a classe do seu objeto 'pl_MDB_2017'?
+
+
+# 1c. Informe (como comentário) qual o tipo dessa variável
+## R:
+
+
+# 1d. Calcule a média da quantidade de pl's do MDB em 2017 e 2018
+# sabendo que, em 2018, foram 33
+
+
+# 2. -----
+
+# Leia o script abaixo e comente com as hashtags o que cada código quer dizer:
+
+
+# 2a.
+senador <- 334
+#
+
+# 2b.
+class(senador)
+#
+
+# 2.c
+name <- "Flavio"
+#
+
+# 2.d
+class(senador) != class(name)
+#
+
+
+# 3. ---- 
+
+# Reescreva a expressão abaixo utilizando o %>% (pipes).
+
+round(mean(divide_by(sum(1:10),3)),digits = 1)
+
+# 4. ----
+
+# Utilizando a função elections_tse() do pacote electionsBR
+# escreva um objeto com os dados de candidatos em 2018:
+# Dica: use os argumentos year=2018,type="candidate"  
---
+++ listas/Lista03.R
@@ -0,0 +1,6 @@
+# Instruções
+# 1. Indicar um conjunto de dados à sua escolha e ler este conjunto de dados no R
+# 2. Realizar alguma manipulação em dplyr/tidyr neste conjunto de dados que envolva os conteúdos tratados em aula.
+# 3. Apresentar uma saída gráfica em ggplot (dito simples, fazer um gráfico) contendo ao menos uma escala de tamanho, cor, preenchimento etc. e utilizando algum tema (qualquer tema, inclusive os nativos do ggplot). A escolha do tipo de gráfico (geometria) também é livre. Comente em seu código, usando #, a razão da escolha deste gráfico.
+# 4. Disponibilizar este material em um arquivo .pptx produzido a partir de um arquivo .rmd (RMarkdown).
+# Não esqueça de anexar à tarefa seu .rmd, seu pptx e, se for o caso, seu conjunto de dados. 
